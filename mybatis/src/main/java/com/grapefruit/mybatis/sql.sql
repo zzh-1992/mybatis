0bfe6ac0e7280228c80369540b20886f4eacf68a
zhihuangzhang <ezzh1992@icloud.com>
2022-08-21 07:32:12 +0800

    表grape字段调整

--- mybatis/src/main/java/com/grapefruit/mybatis/sql.sql
+++ mybatis/src/main/java/com/grapefruit/mybatis/sql.sql
@@ -26,16 +26,16 @@ create table t_student_class
         unique (s_id, c_id)
 );
 
-INSERT INTO jpa.t_class (id, name) VALUES (1, 'chinese语文');
-INSERT INTO jpa.t_class (id, name) VALUES (2, 'math数学');
-INSERT INTO jpa.t_class (id, name) VALUES (3, 'english英语');
+INSERT INTO grapefruit.t_class (id, name) VALUES (1, 'chinese语文');
+INSERT INTO grapefruit.t_class (id, name) VALUES (2, 'math数学');
+INSERT INTO grapefruit.t_class (id, name) VALUES (3, 'english英语');
 
-INSERT INTO jpa.t_student (id, age, class_id, name, favourite, `limit`) VALUES (37, 22, '2', 'ZZH', null, null);
-INSERT INTO jpa.t_student (id, age, class_id, name, favourite, `limit`) VALUES (38, 22, '2', 'ZZH', '[{"id":1,"name":"chinese语文"},{"id":2,"name":"math数学"},{"id":3,"name":"english英语"}]', null);
+INSERT INTO grapefruit.t_student (id, age, class_id, name, favourite, `up_limit`) VALUES (37, 22, '2', 'ZZH', null, null);
+INSERT INTO grapefruit.t_student (id, age, class_id, name, favourite, `up_limit`) VALUES (38, 22, '2', 'ZZH', '[{"id":1,"name":"chinese语文"},{"id":2,"name":"math数学"},{"id":3,"name":"english英语"}]', null);
 
-INSERT INTO jpa.t_student_class (id, s_id, c_id) VALUES (47, 37, 1);
-INSERT INTO jpa.t_student_class (id, s_id, c_id) VALUES (48, 37, 2);
-INSERT INTO jpa.t_student_class (id, s_id, c_id) VALUES (49, 37, 3);
-INSERT INTO jpa.t_student_class (id, s_id, c_id) VALUES (50, 38, 1);
-INSERT INTO jpa.t_student_class (id, s_id, c_id) VALUES (51, 38, 2);
-INSERT INTO jpa.t_student_class (id, s_id, c_id) VALUES (52, 38, 3);
+INSERT INTO grapefruit.t_student_class (id, s_id, c_id) VALUES (47, 37, 1);
+INSERT INTO grapefruit.t_student_class (id, s_id, c_id) VALUES (48, 37, 2);
+INSERT INTO grapefruit.t_student_class (id, s_id, c_id) VALUES (49, 37, 3);
+INSERT INTO grapefruit.t_student_class (id, s_id, c_id) VALUES (50, 38, 1);
+INSERT INTO grapefruit.t_student_class (id, s_id, c_id) VALUES (51, 38, 2);
+INSERT INTO grapefruit.t_student_class (id, s_id, c_id) VALUES (52, 38, 3);
